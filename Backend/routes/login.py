35c27bb2c647019e91aa37d4c8ed64bc2a8f6951
antonio160698 <antonio.1606.98@gmail.com>
2020-10-13 11:14:51 -0500

    se hizo el sensor, encriptacion y sesion

--- Backend/routes/login.py
+++ Backend/routes/login.py
@@ -1,42 +1,45 @@
 from BClima import app, mydb
-from flask import jsonify, request, render_template
+from flask import jsonify, request, render_template, session
 import mysql.connector
-import hashlib, binascii
-#se genero la llave
-#from cryptography.fernet import Fernet
-#import base64
-#from cryptography.hazmat.backends import default_backend
-#from cryptography.hazmat.primitives import hashes
-#from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-#key = Fernet.generate_key()
-#print(key)
-#file = open('key.key', 'wb')  # Open the file as wb to write bytes
-#file.write(key)  # The key is type bytes still
-#file.close()
+import bcrypt
 
 @app.route('/login')
 def login():
     return render_template('hello.html')
+
+@app.route('/okey')
+def okey():
+    if session['clima'] == app.config['SECRET_KEY']:
+        return render_template('hello.html')
+    else:
+        return jsonify(False)
 
 @app.route("/postRegistro", methods=["POST","GET"])
 def postRegistro():
     data = request.form.to_dict()
     email = data['email'] 
     name = data['name']
-    passw = data['pass']
-    print(cripassword(passw) == cripassword(passw))
-    passw = cripassword(passw)
+    passw = str(data['pass'])+','+str(data['email'])
+    passw = hapassword(passw.encode('utf-8')).decode('utf-8')
     try:
         cnx = mysql.connector.connect(**mydb)
         cur = cnx.cursor()
+        cur1 = cnx.cursor(buffered=True)
         try:
-            query = "INSERT INTO Users (Users_Name,Users_Correo,Users_Password) VALUES ('%s', '%s', '%s');"%(name, email, passw)
-            cur.execute(query)
-            cnx.commit()
-            return jsonify({'msg':str(data)})
+            query1 = "SELECT COUNT('x') FROM Users WHERE Users_Correo = '%s';"%(email)
+            cur1.execute(query1)
+            result = [x for x in cur1][0][0]
+            if result < 1 :
+                query = "INSERT INTO Users (Users_Name,Users_Correo,password_b) VALUES ('%s', '%s', '%s');"%(name, email, passw)
+                cur.execute(query)
+                cnx.commit()
+                return jsonify({'msg':str(data)})
+            else:
+                return jsonify({'msg':'ERROR:Ya existe este correo registrado'})
         except Exception as e:
             return jsonify({'msg':'ERROR:'+str(e)})
         finally:
+            cur1.close()
             cur.close()
             cnx.close()
     except Exception as e:
@@ -53,13 +56,11 @@ def getRegistro():
         arr = []
         nombres = ["Id","Nombre", "Correo", "Password"]
         try:
-            cur.execute("SELECT * FROM Users WHERE Users_Correo="+email+";")
+            cur.execute("SELECT users_id, users_name, users_correo, password_b FROM Users WHERE Users_Correo="+email+";")
             result = [dict(zip(nombres,x)) for x in cur]
-            contra = cripassword(contra)
-            print(result[0]['Password'].encode('utf-8').decode('hex'))
-            print(contra.encode('utf-8').decode('hex'))
-            print()
-            if result[0]['Password'].encode('utf-8') == contra.encode('utf-8'):
+            contra = hapassword(contra.encode('utf-8'), result[0]['Password'])
+            if contra:
+                session['clima'] = app.config['SECRET_KEY']
                 return jsonify(True)
             else:
                 return jsonify(False)
@@ -71,13 +72,10 @@ def getRegistro():
     except Exception as e:
         return jsonify({'msg':str(e)})
 
-def cripassword(password_orig):
-    key = hashlib.pbkdf2_hmac(
-        'sha224',
-        password_orig.encode('utf-8'),
-        b'FabricaSoftware$2020',
-        100000,
-        dklen=128
-    )
-    hashpass = binascii.hexlify(key)
-    return hashpass.decode('utf-8')
+def hapassword(password_orig, password_h=None):
+    if(password_h == None):
+        hashed = bcrypt.hashpw(password_orig, bcrypt.gensalt())
+        return hashed
+    else:
+        conf = bcrypt.checkpw(password_orig, password_h)
+        return conf
--- Backend/run.bat
+++ Backend/run.bat
@@ -1,4 +1,4 @@
 pip install mysql-connector-python
 pip install flask
-pip install cryptography
+pip install bcrypt
 pause
