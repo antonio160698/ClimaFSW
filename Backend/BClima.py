cd1b03c56022bd77909fecd5dc7ca5591d3e5414
antonio160698 <antonio.1606.98@gmail.com>
2021-09-19 18:51:32 -0500

    modifique BClima para que corriera

--- Backend/BClima.py
+++ Backend/BClima.py
@@ -5,7 +5,7 @@ import flask.json
 import os
 import threading
 import time
-from clases.sensor import *
+#from clases.sensor import *
 
 class MyJSONEncoder(flask.json.JSONEncoder):
     def default(self, obj):
@@ -27,12 +27,12 @@ app.config['SECRET_KEY']="FabricaSoftware$FIME$2020"
 app.json_encoder = MyJSONEncoder
 
 from routes.route import *
-from routes.login import *
-from routes.sensor import *
+#from routes.login import *
+#from routes.sensor import *
 
 if __name__=="__main__":
-    t1 = threading.Thread(target = asincTemHum)
-    t1.start()
+    #t1 = threading.Thread(target = asincTemHum)
+    #t1.start()
     app.debug = True
     host = os.environ.get('IP','127.0.0.1')
     port = int(os.environ.get('PORT',5000))
