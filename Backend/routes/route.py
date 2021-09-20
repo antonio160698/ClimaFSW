c10eba300472fba11b8c578816faa5dc405fcdb9
antonio160698 <antonio.1606.98@gmail.com>
2021-09-20 00:40:13 -0500

    Se agrego la pantalla de agregar sensor y se actualizaron las otras pantallas

--- Backend/routes/route.py
+++ Backend/routes/route.py
@@ -6,10 +6,17 @@ def index():
     y = []
     for x in range(10):
         temp = random.randint(0, 40)
-        hum = random.randint(1050, 1150)
+        hum = random.randint(0, 100)
         algo = {'Temperatura':temp, 'Humedad':hum}
         y.append(algo)
     return jsonify(y)
+
+@app.route('/api/temphum')
+def temphum():
+    temp = random.randint(0, 40)
+    hum = random.randint(0, 100)
+    algo = {'Temperatura':temp, 'Humedad':hum}
+    return jsonify(algo)
 
 @app.route('/')
 def home():
@@ -18,3 +25,7 @@ def home():
 @app.route('/Registros')
 def registro():
     return render_template('Registro.html')
+
+@app.route('/AgregarSensor')
+def agregarSensor():
+    return render_template('ASensor.html')
